3165ab0a9f122618d41c09059ca24c0da2ac4e8f
atikadwi <atika_dwi_27rpl@student.smktelkom-mlg.sch.id>
2020-12-12 20:51:20 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <first msg="First Component"></first>
+  </div>
+</template>
+
+<script>
+import First from './components/First'
+
+export default {
+  name: 'App',
+  components: {
+    First
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
